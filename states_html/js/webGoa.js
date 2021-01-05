14e2d854640991ed9ed999368da7e51584be8eb5
Sanjana001 <sanjana0056.cse19@chitkara.edu.in>
2021-01-05 16:22:34 +0530

    javascript file for Goa

---
+++ states_html/js/webGoa.js
@@ -0,0 +1,195 @@
+function f1(){document.getElementById("ab").style.backgroundColor="grey";document.getElementById("ac").style.backgroundColor="lightgrey";document.getElementById("ad").style.backgroundColor="lightgrey";}
+
+function f2(){document.getElementById("ab").style.backgroundColor="lightgrey";document.getElementById("ac").style.backgroundColor="grey";document.getElementById("ad").style.backgroundColor="lightgrey";}
+
+function f3(){document.getElementById("ab").style.backgroundColor="lightgrey";document.getElementById("ad").style.backgroundColor="grey";document.getElementById("ac").style.backgroundColor="lightgrey";}
+
+function f8(){document.getElementsByClassName("s3")[0].style.color="white";document.getElementsByClassName("s3")[0].style.backgroundColor="red";document.getElementsByClassName("s3")[0].style.fontWeight="bold";document.getElementsByClassName("s3")[0].style.borderRadius="38%";document.getElementsByClassName("s3")[0].innerHTML="PAYMENT";}
+
+function f9(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function f6(){document.getElementsByClassName("s2")[0].style.color="white";document.getElementsByClassName("s2")[0].style.backgroundColor="red";document.getElementsByClassName("s2")[0].style.fontWeight="bold";document.getElementsByClassName("s2")[0].style.borderRadius="38%";document.getElementsByClassName("s2")[0].innerHTML="PAYMENT";}
+
+function f7(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function f4(){document.getElementsByClassName("s1")[0].style.color="white";document.getElementsByClassName("s1")[0].style.backgroundColor="red";document.getElementsByClassName("s1")[0].style.fontWeight="bold";document.getElementsByClassName("s1")[0].style.borderRadius="38%";document.getElementsByClassName("s1")[0].innerHTML="PAYMENT";}
+
+function f5(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function f0(){document.getElementsByClassName("s")[0].style.color="white";document.getElementsByClassName("s")[0].style.backgroundColor="red";document.getElementsByClassName("s")[0].style.fontWeight="bold";document.getElementsByClassName("s")[0].style.borderRadius="38%";document.getElementsByClassName("s")[0].innerHTML="PAYMENT";}
+
+function ff0(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function ff1(){document.getElementsByClassName("s0")[0].style.color="white";document.getElementsByClassName("s0")[0].style.backgroundColor="red";document.getElementsByClassName("s0")[0].style.fontWeight="bold";document.getElementsByClassName("s0")[0].style.borderRadius="38%";document.getElementsByClassName("s0")[0].innerHTML="PAYMENT";}
+
+function ff2(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function ff3(){document.getElementsByClassName("p0")[0].style.color="white";document.getElementsByClassName("p0")[0].style.backgroundColor="red";document.getElementsByClassName("p0")[0].style.fontWeight="bold";document.getElementsByClassName("p0")[0].style.borderRadius="38%";document.getElementsByClassName("p0")[0].innerHTML="PAYMENT";}
+
+function ff4(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function ff5(){document.getElementsByClassName("p1")[0].style.color="white";document.getElementsByClassName("p1")[0].style.backgroundColor="red";document.getElementsByClassName("p1")[0].style.fontWeight="bold";document.getElementsByClassName("p1")[0].style.borderRadius="38%";document.getElementsByClassName("p1")[0].innerHTML="PAYMENT";}
+
+function ff6(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function ff7(){document.getElementsByClassName("p2")[0].style.color="white";document.getElementsByClassName("p2")[0].style.backgroundColor="red";document.getElementsByClassName("p2")[0].style.fontWeight="bold";document.getElementsByClassName("p2")[0].style.borderRadius="38%";document.getElementsByClassName("p2")[0].innerHTML="PAYMENT";}
+
+function ff8(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function ff9(){document.getElementsByClassName("p3")[0].style.color="white";document.getElementsByClassName("p3")[0].style.backgroundColor="red";document.getElementsByClassName("p3")[0].style.fontWeight="bold";document.getElementsByClassName("p3")[0].style.borderRadius="38%";document.getElementsByClassName("p3")[0].innerHTML="PAYMENT";}
+
+function ff10(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function fff(){document.getElementsByClassName("p4")[0].style.color="white";document.getElementsByClassName("p4")[0].style.backgroundColor="red";document.getElementsByClassName("p4")[0].style.fontWeight="bold";document.getElementsByClassName("p4")[0].style.borderRadius="38%";document.getElementsByClassName("p4")[0].innerHTML="PAYMENT";}
+
+function fff0(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function fff1(){document.getElementsByClassName("p5")[0].style.color="white";document.getElementsByClassName("p5")[0].style.backgroundColor="red";document.getElementsByClassName("p5")[0].style.fontWeight="bold";document.getElementsByClassName("p5")[0].style.borderRadius="38%";document.getElementsByClassName("p5")[0].innerHTML="PAYMENT";}
+
+function fff2(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function fff3(){document.getElementsByClassName("p6")[0].style.color="white";document.getElementsByClassName("p6")[0].style.backgroundColor="red";document.getElementsByClassName("p6")[0].style.fontWeight="bold";document.getElementsByClassName("p6")[0].style.borderRadius="38%";document.getElementsByClassName("p6")[0].innerHTML="PAYMENT";}
+
+function fff4(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c0(){document.getElementsByClassName("c")[0].style.color="white";document.getElementsByClassName("c")[0].style.backgroundColor="red";document.getElementsByClassName("c")[0].style.fontWeight="bold";document.getElementsByClassName("c")[0].style.borderRadius="38%";document.getElementsByClassName("c")[0].innerHTML="PAYMENT";}
+
+function c1(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c2(){document.getElementsByClassName("c1")[0].style.color="white";document.getElementsByClassName("c1")[0].style.backgroundColor="red";document.getElementsByClassName("c1")[0].style.fontWeight="bold";document.getElementsByClassName("c1")[0].style.borderRadius="38%";document.getElementsByClassName("c1")[0].innerHTML="VIEW DETAILS";}
+
+function c3(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c4(){document.getElementsByClassName("c2")[0].style.color="white";document.getElementsByClassName("c2")[0].style.backgroundColor="red";document.getElementsByClassName("c2")[0].style.fontWeight="bold";document.getElementsByClassName("c2")[0].style.borderRadius="38%";document.getElementsByClassName("c2")[0].innerHTML="VIEW DETAILS";}
+
+function c5(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c6(){document.getElementsByClassName("c3")[0].style.color="white";document.getElementsByClassName("c3")[0].style.backgroundColor="red";document.getElementsByClassName("c3")[0].style.fontWeight="bold";document.getElementsByClassName("c3")[0].style.borderRadius="38%";document.getElementsByClassName("c3")[0].innerHTML="VIEW DETAILS";}
+
+function c7(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c8(){document.getElementsByClassName("c4")[0].style.color="white";document.getElementsByClassName("c4")[0].style.backgroundColor="red";document.getElementsByClassName("c4")[0].style.fontWeight="bold";document.getElementsByClassName("c4")[0].style.borderRadius="38%";document.getElementsByClassName("c4")[0].innerHTML="VIEW DETAILS";}
+
+function c9(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c10(){document.getElementsByClassName("c5")[0].style.color="white";document.getElementsByClassName("c5")[0].style.backgroundColor="red";document.getElementsByClassName("c5")[0].style.fontWeight="bold";document.getElementsByClassName("c5")[0].style.borderRadius="38%";document.getElementsByClassName("c5")[0].innerHTML="VIEW DETAILS";}
+
+function c11(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c12(){document.getElementsByClassName("c6")[0].style.color="white";document.getElementsByClassName("c6")[0].style.backgroundColor="red";document.getElementsByClassName("c6")[0].style.fontWeight="bold";document.getElementsByClassName("c6")[0].style.borderRadius="38%";document.getElementsByClassName("c6")[0].innerHTML="VIEW DETAILS";}
+
+function c13(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c14(){document.getElementsByClassName("c7")[0].style.color="white";document.getElementsByClassName("c7")[0].style.backgroundColor="red";document.getElementsByClassName("c7")[0].style.fontWeight="bold";document.getElementsByClassName("c7")[0].style.borderRadius="38%";document.getElementsByClassName("c7")[0].innerHTML="VIEW DETAILS";}
+
+function c15(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function myfunction(){
+  document.getElementById("btn").style.color="red";document.getElementById("btn1").style.color="black";document.getElementById("btn2").style.color="black";document.getElementById("btn3").style.color="black";document.getElementById("btn4").style.color="black";document.getElementById("btn5").style.color="black";
+  document.getElementById("btn").style.fontFamily = "cursive";
+  document.getElementById("btn").style.fontSize = "18px";
+  var str = "Popularly referred to as";
+  var str1 = "Rome of the East and Pearl of the Orient";
+  var str2 = ", the small state of Goa nestles on the lush green Konkan coast on the western coast of India. "
+  var str3 = "Goa is renowned for its breathtaking scenic beauty and the architectural splendors of Old Portuguese mansions magnificent churches, massive forts, and ornate temples.";
+  var str4 = "Commanded by various dynasties, this exotic land is a potpourri of various cultures and traditions.<br><br>";
+  var str5 = "Known for its ";
+  var str6 = "palm-fringed beaches, culinary delights, and lively parties";
+  var str7 = ",Goa amazes everyone with its vibrant lifestyle and gracious hospitality that have made it the most popular tourist destination with travelers around the world. ";
+  document.getElementById("demo").innerHTML=str+str1.fontcolor("indigo")+str2+str3.fontcolor("red")+str4+str5.fontcolor("green")+str6+str7.fontcolor("purple");
+}
+
+function myfunction1(){
+  document.getElementById("btn1").style.color="red";document.getElementById("btn2").style.color="black";document.getElementById("btn3").style.color="black";document.getElementById("btn4").style.color="black";document.getElementById("btn5").style.color="black";document.getElementById("btn").style.color="black"
+  document.getElementById("btn1").style.fontFamily = "cursive";
+  document.getElementById("btn1").style.fontSize = "18px";
+  var str = "Tourism in Goa is a primary industry, as it draws a major amount of foreign exchange to the state. Goa has made a significant place for itself as the most visited tourist destination in the world. "
+  var str1 = "Goa Tourism attracts millions of tourists annually, thanks to its amazing natural beauty and colorful festivals that are well-renowned.<br><br>";
+   var str2 = "In order to promote tourism in Goa, ";
+   var str3 = "the Goa Tourism Development Corporation organizes well-planned city tours, beach tours, heritage tours, pilgrimage tours, and also conducts cultural programs, and shows for tourists. ";
+   var str4 = "The economical holiday packages of Goa Tourism cover all major tourist attractions of the state. Goa tourism Guide available at Goa Tourism reception centers provide information about Goa hotels, Goa attractions, Goa transportation, Goa festivals, Goa shopping and other travel details.";
+  document.getElementById("demo").innerHTML=str+str1.fontcolor("green")+str2+str3.fontcolor("red")+str4;
+}
+
+function myfunction2(){
+  document.getElementById("btn2").style.color="red";document.getElementById("btn").style.color="black";document.getElementById("btn1").style.color="black";document.getElementById("btn3").style.color="black";document.getElementById("btn4").style.color="black";document.getElementById("btn5").style.color="black"
+  document.getElementById("btn2").style.fontFamily = "cursive";
+  document.getElementById("btn2").style.fontSize = "18px";
+  var str = "The ideal time to visit Goa is between the months of";
+  var str1 = "October to March ";
+  var str2 = "when the state enjoys a salubrious climate."
+  document.getElementById("demo").innerHTML=str+str1.fontcolor("darkmagenta")+str2;
+}
+
+function myfunction3(){
+  document.getElementById("btn3").style.color="red";document.getElementById("btn").style.color="black";document.getElementById("btn1").style.color="black";document.getElementById("btn2").style.color="black";document.getElementById("btn4").style.color="black";document.getElementById("btn5").style.color="black"
+  document.getElementById("btn3").style.fontFamily = "cursive";
+  document.getElementById("btn3").style.fontSize = "18px";
+  var str = "Goa is dotted with white sandy beaches, churches, temples, forts and other historical monuments. ";
+  var str1 = "Some of the major tourist attractions in Goa are ";
+  var str2 = "Calangute Beach, Agauda Fort, Manguesh temple, Anjuna Beach, Colva beach, Dona Paula, Colva Beach, Benaulim Beach, Basilica of Bom Jesus, Saint Augustine Church, Chapora Fort, Spice Plantation Garden Dudhsagar fall and Mayhem Lake. ";
+  var str1 = " are some of the major attractions. Not to miss the ";
+  var str2 = "Jaisalmer fort, Mehrangarh Fort in Jodhpur";
+  document.getElementById("demo").innerHTML=str.fontcolor("red")+str1+str2.fontcolor("purple");
+}
+
+function myfunction4(){
+  document.getElementById("btn4").style.color="red";document.getElementById("btn").style.color="black";document.getElementById("btn1").style.color="black";document.getElementById("btn2").style.color="black";document.getElementById("btn3").style.color="black";document.getElementById("btn5").style.color="black"
+  document.getElementById("btn4").style.fontFamily = "cursive";
+  document.getElementById("btn4").style.fontSize = "18px";
+  var str = "By Air : Dabolim Airport of Goa is situated at a distance of 29 Kms from the capital.Popular Flights Routes to Goa,Delhi to Goa Flight,Mumbai to Goa Flight,Chennai to Goa Flight,Bangalore to Goa Flight,Hyderabad to Goa Flight<br><br>";
+
+var str2 = "By Rail : "
+var str3 = "Goa's two main stations are at Margao and Vasco da Gama<br><br>";
+
+var str4 = "By Road";
+var str5 = " : Panjim, the capital city is linked by road to Mumbai, Bangalore, Belgaum, Gokarn, Hospet , Kolhapur, Pune , Mysore and many other cities of the neighboring states.<br><br>";
+
+var str6 = "By Sea :";
+var str7 = "Ship services between Bombay and Goa organized by Damania. Several ferry services are also available";
+  document.getElementById("demo").innerHTML=str.bold()+str1.fontcolor("grey")+str2.bold()+str3.fontcolor("grey")+str4.bold()+str5.fontcolor("grey")+str6.bold()+str7.fontcolor("grey");
+}
+
+function myfunction5(){
+  document.getElementById("btn5").style.color="red";document.getElementById("btn").style.color="black";document.getElementById("btn1").style.color="black";document.getElementById("btn2").style.color="black";document.getElementById("btn3").style.color="black";document.getElementById("btn4").style.color="black"
+  document.getElementById("btn5").style.fontFamily = "cursive";
+  document.getElementById("btn5").style.fontSize = "18px";
+  var str = "Goa has many options varying from bikes to cars to ferries to buses, which will make your travel not only faster but also convenient. <br><br>";
+  var str1 = "The best part about this place is that even though the distances between places is significant, you can easily rent a bike or car to cover that distance comfortably. ";
+
+  document.getElementById("demo").innerHTML=str+str1.fontcolor("red");
+}
+
+function myfunction6(){
+  document.getElementById("bt").style.color="blue";;document.getElementById("bt1").style.color="black";document.getElementById("bt2").style.color="black"
+  document.getElementById("bt").style.fontFamily = "cursive";
+  document.getElementById("bt").style.fontSize = "18px";
+  var str = "The beaches in the far South and the farthest North are the best. ";
+  var str1 = "North Goa usually tends to be ";
+  var str3 = "Anjuna/Vagator area (decent, crowded beaches) and Candolim/Baga (very crowded beaches), and Ashwem/Arambol (fun clean, pretty beaches).";
+  var str4 = "But there are beaches even further North that are totally undeveloped and empty.";
+  document.getElementById("demo1").innerHTML=str+str1.fontcolor("indigo")+str3.fontcolor("red")+str4;
+}
+
+function myfunction7(){
+ document.getElementById("bt1").style.color="blue";;document.getElementById("bt").style.color="black";document.getElementById("bt2").style.color="black"
+  document.getElementById("bt1").style.fontFamily = "cursive";
+  document.getElementById("bt1").style.fontSize = "18px";
+  var str = "The best time to visit Goa is in the";
+  var str0 = "winter months ";
+  var str3 = "or in between the months of "
+    var str1 = "November to February"
+    var str2 = " when the weather is pleasant and cool.";
+  document.getElementById("demo1").innerHTML=str+str0.fontcolor("darkmagenta")+str3+str1.fontcolor("olive")+str2;
+}
+
+function myfunction8(){
+  document.getElementById("bt2").style.color="blue";;document.getElementById("bt").style.color="black";document.getElementById("bt1").style.color="black"
+  document.getElementById("bt2").style.fontFamily = "cursive";
+  document.getElementById("bt2").style.fontSize = "18px";
+  var str = "Anjuna Beach, Baga Beach, Sinquerism in Beaches";
+  var str1 = " are some of the major attractions. Not to miss the ";
+  var str2 = "Cuisine of Goa";
+  var str3 = ", that invite tourists from far and wide.";
+  document.getElementById("demo1").innerHTML=str.fontcolor("red")+str1+str2.fontcolor("purple")+str3;
+}
