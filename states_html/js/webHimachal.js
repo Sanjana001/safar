e6bb28e3c72fcf25821015a19409fbfe98dd0f6b
Sanjana001 <sanjana0056.cse19@chitkara.edu.in>
2021-01-07 13:55:58 +0530

    javascript file for Himachal

---
+++ states_html/js/webHimachal.js
@@ -0,0 +1,193 @@
+function f1(){document.getElementById("ab").style.backgroundColor="grey";document.getElementById("ac").style.backgroundColor="lightgrey";document.getElementById("ad").style.backgroundColor="lightgrey";document.getElementById("ae").style.backgroundColor="lightgrey";document.getElementById("af").style.backgroundColor="lightgrey";}
+
+function f2(){document.getElementById("ab").style.backgroundColor="lightgrey";document.getElementById("ac").style.backgroundColor="grey";document.getElementById("ad").style.backgroundColor="lightgrey";document.getElementById("ae").style.backgroundColor="lightgrey";document.getElementById("af").style.backgroundColor="lightgrey";}
+
+function f3(){document.getElementById("ab").style.backgroundColor="lightgrey";document.getElementById("ad").style.backgroundColor="grey";document.getElementById("ac").style.backgroundColor="lightgrey";document.getElementById("ae").style.backgroundColor="lightgrey";document.getElementById("af").style.backgroundColor="lightgrey";}
+
+function a(){document.getElementById("ab").style.backgroundColor="lightgrey";document.getElementById("ac").style.backgroundColor="lightgrey";document.getElementById("ad").style.backgroundColor="lightgrey";document.getElementById("ae").style.backgroundColor="grey";document.getElementById("af").style.backgroundColor="lightgrey";}
+
+function b(){document.getElementById("ab").style.backgroundColor="lightgrey";document.getElementById("ac").style.backgroundColor="lightgrey";document.getElementById("ad").style.backgroundColor="lightgrey";document.getElementById("ae").style.backgroundColor="lightgrey";document.getElementById("af").style.backgroundColor="grey";}
+
+function f8(){document.getElementsByClassName("s3")[0].style.color="white";document.getElementsByClassName("s3")[0].style.backgroundColor="red";document.getElementsByClassName("s3")[0].style.fontWeight="bold";document.getElementsByClassName("s3")[0].style.borderRadius="38%";document.getElementsByClassName("s3")[0].innerHTML="PAYMENT";}
+
+function f9(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function f6(){document.getElementsByClassName("s2")[0].style.color="white";document.getElementsByClassName("s2")[0].style.backgroundColor="red";document.getElementsByClassName("s2")[0].style.fontWeight="bold";document.getElementsByClassName("s2")[0].style.borderRadius="38%";document.getElementsByClassName("s2")[0].innerHTML="PAYMENT";}
+
+function f7(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function f4(){document.getElementsByClassName("s1")[0].style.color="white";document.getElementsByClassName("s1")[0].style.backgroundColor="red";document.getElementsByClassName("s1")[0].style.fontWeight="bold";document.getElementsByClassName("s1")[0].style.borderRadius="38%";document.getElementsByClassName("s1")[0].innerHTML="PAYMENT";}
+
+function f5(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function f0(){document.getElementsByClassName("s")[0].style.color="white";document.getElementsByClassName("s")[0].style.backgroundColor="red";document.getElementsByClassName("s")[0].style.fontWeight="bold";document.getElementsByClassName("s")[0].style.borderRadius="38%";document.getElementsByClassName("s")[0].innerHTML="PAYMENT";}
+
+function ff0(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function ff1(){document.getElementsByClassName("s0")[0].style.color="white";document.getElementsByClassName("s0")[0].style.backgroundColor="red";document.getElementsByClassName("s0")[0].style.fontWeight="bold";document.getElementsByClassName("s0")[0].style.borderRadius="38%";document.getElementsByClassName("s0")[0].innerHTML="PAYMENT";}
+
+function ff2(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function ff3(){document.getElementsByClassName("p0")[0].style.color="white";document.getElementsByClassName("p0")[0].style.backgroundColor="red";document.getElementsByClassName("p0")[0].style.fontWeight="bold";document.getElementsByClassName("p0")[0].style.borderRadius="38%";document.getElementsByClassName("p0")[0].innerHTML="PAYMENT";}
+
+function ff4(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function ff5(){document.getElementsByClassName("p1")[0].style.color="white";document.getElementsByClassName("p1")[0].style.backgroundColor="red";document.getElementsByClassName("p1")[0].style.fontWeight="bold";document.getElementsByClassName("p1")[0].style.borderRadius="38%";document.getElementsByClassName("p1")[0].innerHTML="PAYMENT";}
+
+function ff6(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function ff7(){document.getElementsByClassName("p2")[0].style.color="white";document.getElementsByClassName("p2")[0].style.backgroundColor="red";document.getElementsByClassName("p2")[0].style.fontWeight="bold";document.getElementsByClassName("p2")[0].style.borderRadius="38%";document.getElementsByClassName("p2")[0].innerHTML="PAYMENT";}
+
+function ff8(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function ff9(){document.getElementsByClassName("p3")[0].style.color="white";document.getElementsByClassName("p3")[0].style.backgroundColor="red";document.getElementsByClassName("p3")[0].style.fontWeight="bold";document.getElementsByClassName("p3")[0].style.borderRadius="38%";document.getElementsByClassName("p3")[0].innerHTML="PAYMENT";}
+
+function ff10(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function fff(){document.getElementsByClassName("p4")[0].style.color="white";document.getElementsByClassName("p4")[0].style.backgroundColor="red";document.getElementsByClassName("p4")[0].style.fontWeight="bold";document.getElementsByClassName("p4")[0].style.borderRadius="38%";document.getElementsByClassName("p4")[0].innerHTML="PAYMENT";}
+
+function fff0(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function fff1(){document.getElementsByClassName("p5")[0].style.color="white";document.getElementsByClassName("p5")[0].style.backgroundColor="red";document.getElementsByClassName("p5")[0].style.fontWeight="bold";document.getElementsByClassName("p5")[0].style.borderRadius="38%";document.getElementsByClassName("p5")[0].innerHTML="PAYMENT";}
+
+function fff2(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function fff3(){document.getElementsByClassName("p6")[0].style.color="white";document.getElementsByClassName("p6")[0].style.backgroundColor="red";document.getElementsByClassName("p6")[0].style.fontWeight="bold";document.getElementsByClassName("p6")[0].style.borderRadius="38%";document.getElementsByClassName("p6")[0].innerHTML="PAYMENT";}
+
+function fff4(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c0(){document.getElementsByClassName("c")[0].style.color="white";document.getElementsByClassName("c")[0].style.backgroundColor="red";document.getElementsByClassName("c")[0].style.fontWeight="bold";document.getElementsByClassName("c")[0].style.borderRadius="38%";document.getElementsByClassName("c")[0].innerHTML="PAYMENT";}
+
+function c1(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c2(){document.getElementsByClassName("c1")[0].style.color="white";document.getElementsByClassName("c1")[0].style.backgroundColor="red";document.getElementsByClassName("c1")[0].style.fontWeight="bold";document.getElementsByClassName("c1")[0].style.borderRadius="38%";document.getElementsByClassName("c1")[0].innerHTML="PAYMENT";}
+
+function c3(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c4(){document.getElementsByClassName("c2")[0].style.color="white";document.getElementsByClassName("c2")[0].style.backgroundColor="red";document.getElementsByClassName("c2")[0].style.fontWeight="bold";document.getElementsByClassName("c2")[0].style.borderRadius="38%";document.getElementsByClassName("c2")[0].innerHTML="PAYMENT";}
+
+function c5(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c6(){document.getElementsByClassName("c3")[0].style.color="white";document.getElementsByClassName("c3")[0].style.backgroundColor="red";document.getElementsByClassName("c3")[0].style.fontWeight="bold";document.getElementsByClassName("c3")[0].style.borderRadius="38%";document.getElementsByClassName("c3")[0].innerHTML="PAYMENT";}
+
+function c7(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c8(){document.getElementsByClassName("c4")[0].style.color="white";document.getElementsByClassName("c4")[0].style.backgroundColor="red";document.getElementsByClassName("c4")[0].style.fontWeight="bold";document.getElementsByClassName("c4")[0].style.borderRadius="38%";document.getElementsByClassName("c4")[0].innerHTML="PAYMENT";}
+
+function c9(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c10(){document.getElementsByClassName("c5")[0].style.color="white";document.getElementsByClassName("c5")[0].style.backgroundColor="red";document.getElementsByClassName("c5")[0].style.fontWeight="bold";document.getElementsByClassName("c5")[0].style.borderRadius="38%";document.getElementsByClassName("c5")[0].innerHTML="PAYMENT";}
+
+function c11(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c12(){document.getElementsByClassName("c6")[0].style.color="white";document.getElementsByClassName("c6")[0].style.backgroundColor="red";document.getElementsByClassName("c6")[0].style.fontWeight="bold";document.getElementsByClassName("c6")[0].style.borderRadius="38%";document.getElementsByClassName("c6")[0].innerHTML="PAYMENT";}
+
+function c13(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function c14(){document.getElementsByClassName("c7")[0].style.color="white";document.getElementsByClassName("c7")[0].style.backgroundColor="red";document.getElementsByClassName("c7")[0].style.fontWeight="bold";document.getElementsByClassName("c7")[0].style.borderRadius="38%";document.getElementsByClassName("c7")[0].innerHTML="PAYMENT";}
+
+function c15(x){x.style.color="green";x.style.backgroundColor="white";}
+
+function myfunction(){
+  document.getElementById("btn").style.color="red";document.getElementById("btn1").style.color="black";document.getElementById("btn2").style.color="black";document.getElementById("btn3").style.color="black";document.getElementById("btn4").style.color="black";document.getElementById("btn5").style.color="black";
+  document.getElementById("btn").style.fontFamily = "cursive";
+  document.getElementById("btn").style.fontSize = "18px";
+  var str = "The picturesque state of Himachal Pradesh is nestled between";
+  var str1 = "the snow topped Himalayan ranges in North India";
+  var str2 = ". Known for its spectacular and panoramic landscapes, this traveler's paradise is blessed with ";
+  var str3 = "lofty snow peaks, deep gorges, verdant valleys, thundering rivers, enchanting mountain lakes, flower carpeted meadows, ancient temples, and magnificent monasteries ";
+  var str4 = "steeped in time. Dotted with numerous ";
+  var str5 = "hill stations, Himachal Pradesh proves to be a wonderful location for the tourists who want to relax and simply feel rejuvenated. ";
+  var str6 = "The state also offers amazing opportunities for trekking, mountaineering, para gliding, skiing, golf , river rafting and ice skating";
+  document.getElementById("demo").innerHTML=str+str1.fontcolor("indigo")+str2+str3.fontcolor("red")+str4+str5.fontcolor("green")+str6;
+}
+
+function myfunction1(){
+  document.getElementById("btn1").style.color="red";document.getElementById("btn2").style.color="black";document.getElementById("btn3").style.color="black";document.getElementById("btn4").style.color="black";document.getElementById("btn5").style.color="black";document.getElementById("btn").style.color="black"
+  document.getElementById("btn1").style.fontFamily = "cursive";
+  document.getElementById("btn1").style.fontSize = "18px";
+  var str = "Tourism in Himachal Pradesh has been given high priority by the government of Himachal Pradesh in recent years. ";
+  var str1 = "Renowned for its striking natural beauty, Himachal Pradesh tourism attracts millions of tourists - both domestic and international, who love to spend their vacations in tranquility and serene sylvan surroundings.";
+  var str2 = "<br><br>To attract more tourists, Himachal Pradesh Tourism Department is working towards the betterment of its ";
+  var str3 = "infrastructure, conservation and preservation of historical monuments, and identifying new tourist destinations etc. ";
+  var str4 = "There is a special focus on development of activities-based on tourism in order to increase the duration of the stay of the visitors/tourists."
+  document.getElementById("demo").innerHTML=str+str1.fontcolor("green")+str2+str3.fontcolor("red")+str4;
+}
+
+function myfunction2(){
+  document.getElementById("btn2").style.color="red";document.getElementById("btn").style.color="black";document.getElementById("btn1").style.color="black";document.getElementById("btn3").style.color="black";document.getElementById("btn4").style.color="black";document.getElementById("btn5").style.color="black"
+  document.getElementById("btn2").style.fontFamily = "cursive";
+  document.getElementById("btn2").style.fontSize = "18px";
+  var str = "Himachal Pradesh is known for its salubrious climate. The ideal time to visit Himachal Pradesh is between the months of ";
+  var str1 = "April to June when weather is mild and sunny and is perfect for ";
+  var str2 = "mountaineering or enjoying an idyllic nature walk."
+  document.getElementById("demo").innerHTML=str+str1.fontcolor("darkmagenta")+str2;
+}
+
+function myfunction3(){
+  document.getElementById("btn3").style.color="red";document.getElementById("btn").style.color="black";document.getElementById("btn1").style.color="black";document.getElementById("btn2").style.color="black";document.getElementById("btn4").style.color="black";document.getElementById("btn5").style.color="black"
+  document.getElementById("btn3").style.fontFamily = "cursive";
+  document.getElementById("btn3").style.fontSize = "18px";
+  var str = "Chamba valley, Spiti valley, Kinnaur, Shimla, Manali, Kullu valley, Lahaul, Kangra valley, Mcleodganj, Dalhousie, and Dharamshala ";
+  var str1 = "Himachal Pradesh travel destination which are dotted with ";
+  var str2 = "numerous churches, monasteries, temples, forts and other significant monuments.";
+  document.getElementById("demo").innerHTML=str.fontcolor("red")+str1+str2.fontcolor("purple");
+}
+
+function myfunction4(){
+  document.getElementById("btn4").style.color="red";document.getElementById("btn").style.color="black";document.getElementById("btn1").style.color="black";document.getElementById("btn2").style.color="black";document.getElementById("btn3").style.color="black";document.getElementById("btn5").style.color="black"
+  document.getElementById("btn4").style.fontFamily = "cursive";
+  document.getElementById("btn4").style.fontSize = "18px";
+  var str = "By Air : ";
+  var str1 = "Himachal Pradesh has three airports at Gaggal, Bhuntar and Jubbarhatti, which connect the state to Delhi and Chandigarh via regular flights by Indian Airlines, Kingfisher Red, MDLR Airlines, and Jagson Airlines.<br><br>";
+  var str2 = "By Rail : ";
+  var str3 = "Kalka is the most important railway station in Himachal Pradesh. A narrow-gauge railway track connects Kalka with the capital city, Shimla. The longest railway line is from Pathankot in Punjab to Jogindernagar in Himachal Pradesh.<br><br>.";
+  var str4 = "By Road :";
+  var str5 = "Himachal Pradesh has a very good road network. During monsoons, a few roads are closed in fear of landslides. Himachal Pradesh operates deluxe buses and luxury coaches from all prime cities of North India.";
+  document.getElementById("demo").innerHTML=str.bold()+str1.fontcolor("grey")+str2.bold()+str3.fontcolor("grey")+str4.bold()+str5.fontcolor("grey");
+}
+
+function myfunction5(){
+  document.getElementById("btn5").style.color="red";document.getElementById("btn").style.color="black";document.getElementById("btn1").style.color="black";document.getElementById("btn2").style.color="black";document.getElementById("btn3").style.color="black";document.getElementById("btn4").style.color="black"
+  document.getElementById("btn5").style.fontFamily = "cursive";
+  document.getElementById("btn5").style.fontSize = "18px";
+  var str = "Getting around this hilly state is easy if you have your own transportation, be it a car or motorcycle. The State Government buses ply between various destinations in the state are small. ";
+  var str1 = "<br><br>They are driven at a snail's pace because the vehicular machinery can only do so much, but private operators save the day for tourists with many buses and coaches, tours and MUVs.";
+  document.getElementById("demo").innerHTML=str+str1.fontcolor("red");
+}
+
+function myfunction6(){
+  document.getElementById("bt").style.color="blue";document.getElementById("bt1").style.color="black";document.getElementById("bt2").style.color="black"
+  document.getElementById("bt").style.fontFamily = "cursive";
+  document.getElementById("bt").style.fontSize = "18px";
+  var str = "The picturesque state of Himachal Pradesh is nestled between";
+  var str1 = "the snow topped Himalayan ranges in North India";
+  var str2 = ". Known for its spectacular and panoramic landscapes, this traveler's paradise is blessed with ";
+  var str3 = "lofty snow peaks, deep gorges, verdant valleys, thundering rivers, enchanting mountain lakes, flower carpeted meadows, ancient temples, and magnificent monasteries ";
+  var str4 = "steeped in time. Dotted with numerous ";
+  var str5 = "hill stations, Himachal Pradesh proves to be a wonderful location for the tourists who want to relax and simply feel rejuvenated. ";
+  var str6 = "The state also offers amazing opportunities for trekking, mountaineering, para gliding, skiing, golf , river rafting and ice skating";
+  document.getElementById("demo1").innerHTML=str+str1.fontcolor("indigo")+str2+str3.fontcolor("red")+str4+str5.fontcolor("green")+str6;
+}
+
+function myfunction7(){
+  document.getElementById("bt1").style.color="blue";document.getElementById("bt").style.color="black";document.getElementById("bt2").style.color="black"
+  document.getElementById("bt1").style.fontFamily = "cursive";
+  document.getElementById("bt1").style.fontSize = "18px";
+  var str = "The best time to visit Jaipur is in the";
+  var str0 = "winter months ";
+  var str3 = "or in between the months of "
+    var str1 = "November to February"
+    var str2 = " when the weather is pleasant and cool.";
+  document.getElementById("demo1").innerHTML=str+str0.fontcolor("darkmagenta")+str3+str1.fontcolor("olive")+str2;
+}
+
+function myfunction8(){
+  document.getElementById("bt2").style.color="blue";document.getElementById("bt").style.color="black";document.getElementById("bt1").style.color="black"
+  document.getElementById("bt2").style.fontFamily = "cursive";
+  document.getElementById("bt2").style.fontSize = "18px";
+    var str = "Chamba valley, Spiti valley, Kinnaur, Shimla, Manali, Kullu valley, Lahaul, Kangra valley, Mcleodganj, Dalhousie, and Dharamshala ";
+  var str1 = "Himachal Pradesh travel destination which are dotted with ";
+  var str2 = "numerous churches, monasteries, temples, forts and other significant monuments.";
+  document.getElementById("demo1").innerHTML=str.fontcolor("red")+str1+str2.fontcolor("purple");
+}
